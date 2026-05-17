d6ac2a04554d1a71baa6c94ec62fb033d4539df0
Ben S. <bschultz1990@gmail.com>
2026-05-17 16:35:19 -0500

    refine main.go

--- main.go
+++ main.go
@@ -1,36 +1,49 @@
 package main
 
-import(
-	"fmt"
+import (
 	"encoding/json"
+	"fmt"
+	"os"
 	// "regexp"
 )
 
 
-type bookmark struct {
+type bookmarks struct {
 	Url string `json:"url"`
 	Name string `json:"name"`
 }
 
 
+// func importTextLinks () {
+//
+// }
+
+
+// func extractLinkName () {
+//
+// }
+
+
 func main () {
 	// Create a Go struct instance
-	p:= bookmark{
+	p:= bookmarks{
 		Url: "https://www.duckduckgo.com/innerdocument.docx",
 		Name: "innerdocument.docx",
 	}
 
+
 	// Encode (marshal) the struct to JSON
-	jsonData, err := json.Marshal(p)
+	jsonData, err := json.MarshalIndent(p,"","	")
 	if err != nil {
 	  fmt.Println("Error encoding JSON", err)
 	  return
 	}
 	fmt.Println("Encoded JSON:", string(jsonData))
+	os.WriteFile("bookmarks.json", []byte(jsonData), 0666)
 
 
 	// Decode (unmarshal) JSON back into a struct
-	var decodedBookmark bookmark
+	var decodedBookmark bookmarks
 	err = json.Unmarshal(jsonData, &decodedBookmark)
 	if err != nil {
 	  fmt.Println("Error decoding JSON", err)
